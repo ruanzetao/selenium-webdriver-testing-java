078673fe8ddd7c1be6e5bbc1ac6e458ee558a62d
ruanzetao <ruanzetao@gmail.com>
2022-07-10 00:01:27 +0700

    Commit assignment for handle default dropdown

--- src/automationfc/com/Topic_08_Textbox_TextArea.java
+++ src/automationfc/com/Topic_08_Textbox_TextArea.java
@@ -10,37 +10,56 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_00_Template {
+public class Topic_08_Textbox_TextArea {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
-	
-
+	String emailAddress;
+	String loginUrl;
+	String userID;
+	String password;
 	// Format code: Ctrl Sh F
 	@BeforeClass
 	public void beforeClass() {
-		
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-
+		driver.get("https://demo.guru99.com/");
+		loginUrl=driver.getCurrentUrl();
+		//Define variables
+		emailAddress="checkinbysang"+"@mailinator.com";
+		
+		
 	}
 
 	@Test
-	public void TC_01_() {
-		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
-		// Action
-		driver.findElement(By.cssSelector("div.footer a[title='My Account']")).click();
-		String loginPageUrl=driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "http://live.techpanda.org/index.php/customer/account/login/");
-		driver.findElement(By.cssSelector("a[title='Create an Account'")).click();
-		Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+	public void TC_01_Register() {
+		
+		//Find element
+		//Ctrl + Space => Suggest code
+		// Home
+		// End
+		
+//		driver.findElement(By.xpath("//a[text()='here']"));
+		driver.findElement(By.name("emailid")).sendKeys(emailAddress);;
+		driver.findElement(By.name("btnLogin")).click();
+		
+		userID=driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td")).getText();
+		password=driver.findElement(By.xpath("//td[text()='Password :']/following-sibling::td")).getText();
+		
+		
 	}
 
 	@Test
-	public void TC_02_() {
-		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
-
+	public void TC_02_Login() {
+		driver.get(loginUrl);
+		driver.findElement(By.name("uid")).sendKeys(userID);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.name("btnLogin")).click();
+		
+		//Verify home page is displayed
+		driver.findElement(By.cssSelector("tr.heading3>td"));
+		Assert.assertEquals(driver.findElement(By.cssSelector("tr.heading3>td")).getText(), "Manger Id: "+userID);
 	}
 //
 //	@Test
--- src/automationfc/com/Topic_09_Handle_Default_Dropdown.java
+++ src/automationfc/com/Topic_09_Handle_Default_Dropdown.java
@@ -5,20 +5,21 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_00_Template {
+public class Topic_09_Handle_Default_Dropdown {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
-	
+	Select select;
 
 	// Format code: Ctrl Sh F
 	@BeforeClass
 	public void beforeClass() {
-		
+
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -27,21 +28,35 @@ public class Topic_00_Template {
 	}
 
 	@Test
-	public void TC_01_() {
-		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+	public void TC_01_NopCommerce() {
+		driver.get("https://demo.nopcommerce.com/register");
 		// Action
-		driver.findElement(By.cssSelector("div.footer a[title='My Account']")).click();
-		String loginPageUrl=driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "http://live.techpanda.org/index.php/customer/account/login/");
-		driver.findElement(By.cssSelector("a[title='Create an Account'")).click();
-		Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+		driver.findElement(By.id("FirstName")).sendKeys("Nguyen");
+		driver.findElement(By.id("LastName")).sendKeys("Xuan Sang");
+
+		// Dropdown
+		//Khi nao dung thao tac voi element thi moi khoi tao
+		select=new Select(driver.findElement(By.name("DateOfBirthDay")));
+		
+		//Thao tac voi Dropdown
+		select.selectByVisibleText("20");
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), "20");
+		select.isMultiple();
+		
+		select=new Select(driver.findElement(By.name("DateOfBirthMonth")));
+		select.selectByVisibleText("May");
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), "May");
+
+		select=new Select(driver.findElement(By.name("DateOfBirthYear")));
+		select.selectByVisibleText("1992");
+		Assert.assertEquals(select.getFirstSelectedOption().getText(), "1992");
 	}
 
-	@Test
-	public void TC_02_() {
-		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
-
-	}
+//	@Test
+//	public void TC_02_() {
+//		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+//
+//	}
 //
 //	@Test
 //	public void TC_03_() {
